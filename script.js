a7701654a3145a47ebe24221332bd3e2af9dbab0
austin-bagwell <austinbagwell.dev@gmail.com>
2022-10-12 08:53:32 -0400

    adjusted borders, text spacing. added API ability names

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
 "use strict";
 
 // gets API data for a random Pokemon
-
 function getRandomPokemon() {
   const randomPokeID = Math.floor(Math.random() * 899);
   const url = "https://pokeapi.co/api/v2/pokemon/" + randomPokeID;
 
   const pokeName = document.querySelector("#pokemon-name");
-  // const abilityList = document.querySelector(".ability-list");
   const frontSprite = document.querySelector("#front-sprite");
+  const ability1 = document.querySelector("#ability-1");
+  const ability2 = document.querySelector("#ability-2");
+
   fetch(url)
     .then((res) => res.json())
     .then((data) => {
       console.log(`Pokemon promise data: `, data);
       pokeName.innerText = data.name;
       frontSprite.src = data.sprites.front_default;
-      // works only for abilities[0]
-      // let firstAbility = data.abilities[0].ability.name;
-      // let allAbilities = [];
-      // data.abilities.forEach((ability) => allAbilities.push(ability));
-      // console.log(`First Ability: ${firstAbility}`);
-      // console.log(`All ability objects? ${allAbilities}`);
+
+      ability1.innerText = data.abilities[0]?.ability?.name;
+      ability2.innerText = data.abilities[1]?.ability?.name;
     })
     .catch((err) => {
       console.log(`Error ${err}`);
